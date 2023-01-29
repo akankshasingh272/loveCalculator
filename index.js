db7f922d37fe630691947428a7b284e8ab4b9e0c
Akanksha Singh <74486391+akankshasingh272@users.noreply.github.com>
2023-01-29 14:55:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,4 +4,8 @@ prompt("What's your name? ");
 prompt("What's your crush name? ");
 var loveScore = Math.random()*100;
 loveScore = Math.floor(loveScore)+1;
-alert("Your Love Score is "+ loveScore + "%");
+if(loveScore>70){
+  alert("Your love score is "+ loveScore + "%. #TrueLove :}")
+} else{
+  alert("Your Love Score is "+ loveScore + "%.");
+}
