de122d81b9d395636dde841a545dfd69b951d195
Akanksha Singh <74486391+akankshasingh272@users.noreply.github.com>
2023-01-29 09:07:35 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+//Love Calculator
+
+prompt("What's your name? ");
+prompt("What's your crush name? ");
+var loveScore = Math.random()*100;
+loveScore = Math.floor(loveScore)+1;
+alert("Your Love Score is "+ loveScore + "%");
